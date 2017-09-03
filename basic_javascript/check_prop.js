1c2c570fe5ab9ae534e2ac01ba92b196f989eda1
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2017-09-03 11:38:07 -0700

    hasOwnProperty

--- basic_javascript/check_prop.js
+++ basic_javascript/check_prop.js
@@ -1,3 +1,4 @@
+print = require( 'pretty-print');
 // Sometimes it is useful to check if the property of a given object exists or not. We can use the .hasOwnProperty(propname) method of objects to determine if that object has the given property name. .hasOwnProperty() returns true or false if the property is found or not.
 // 
 // Example
@@ -11,9 +12,25 @@
 // Instructions
 // Modify the function checkObj to test myObj for checkProp. If the property is found, return that property's value. If not, return "Not Found".
 
-var calvin = {
-	firstName = "calvin";
-	lastName = "settachatgul";
+var myObj = {
+	firstName : "calvin",
+	lastName : "settachatgul", 
 }
 
+console.log(myObj);
 
+print(myObj);
+
+console.log( myObj.hasOwnProperty("firstName"));
+
+function checObj(checkProp) {
+	if( myObj.hasOwnProperty( checkProp)){
+		return myObj[checkProp];
+	}
+	else{
+		return "Not Found";
+	}
+}
+
+console.log( "firstName",checObj( 'firstName'));
+console.log( "middleName",  checObj( 'middleName'));
